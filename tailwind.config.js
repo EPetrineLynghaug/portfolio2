8c90e12b13fe875e28248db92c0da4f34905df94
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2025-05-31 20:44:42 +0200

    toggle is working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
